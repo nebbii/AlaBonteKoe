ea297f1edaca5a746d8d11e009f6d01d266c4c79
nebbii <ben.wolthuis@gmail.com>
2015-12-03 13:20:02 +0100

    Create config.php

---
+++ include/config.php
@@ -0,0 +1,9 @@
+<?php
+define("HOST", "localhost");
+define("USER", "root");
+define("PASS", "");
+define("DBNAME", "debontekoe");
+
+define("RESTAUREANTMAIL", "restaurant@uitgaanscentrumdebontekoe.nl");
+define("RESTAURANTMAILNAAM", "Restaurant de Bonte Koe");
+?>
