3493f2456c6615cc92b23358854119a9f238c2c7
mwijsman <wijsmannen@gmail.com>
2016-01-15 11:58:40 +0100

    check index2 en de menukaart2

---
+++ index2.php
@@ -0,0 +1,84 @@
+<?php 
+
+include_once "page_navigation.php";
+
+echo getNav();
+?>
+
+<div class="logo_home">
+    <img src="images/Logo/Logo3.png" draggable="false" />
+    <h1>De Bonte Koe</h1>
+</div>
+
+<ul id="slider">
+        <li>
+            <a href="restaurant_home.php">
+                <img src="images/nr3.jpg" width="100%" height="100%" draggable="false"/>
+            </a>
+                <div class="slider-bg-restaurant"></div>
+                <div class="slider-info">
+                        <strong>Het Restaurant</strong>
+                        <p class="slider-text">Dit is het restaurant van de Bonte Koe. Hier zie je het beste koks en de lekkerste eten. Als je naar deze pagina gaat dan kan je ook reserveren en de menukaart zien.</p>
+                </div>
+        </li>
+        <li>
+            <a href="uitgaan_home.php">
+                <img src="images/nr2.jpg"  width="100%" height="100%"  draggable="false"/>
+            </a>
+            <div class="slider-bg-uitgaan"></div>
+                <div class="slider-info">
+                        <strong>Uitgaanscentrum</strong>
+                        <p class="slider-text">Dit is de Uitgaanscentrum van de Bonte Koe, hier kan je kijken welke zalen wij hebben en wat er te beleven is. Er zijn totaal 3 zalen met allemaal een andere stijl. Dus kom een keertje langs om het te beleven!!!</p>
+                </div>
+        </li>
+        <li>
+            <a href="bioscoop_home.php">
+                <img src="images/nr1.jpg"  width="100%" height="100%" draggable="false" />
+            </a>
+                <div class="slider-bg-bioscoop"></div>
+                <div class="slider-info">
+                        <strong>De Bioscoop</strong>
+                        <p class="slider-text">Dit is de Bioscoop van de Bonte Koe, Hier kan je naar de nieuwste films in de beste kwaliteit. Op deze pagina kan je ook gelijk zien hoelaat elke film draait en hoeveel plekken ernog beschikbaar zijn.</p>
+                </div>
+        </li>
+</ul>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="js/jquery.zaccordion.js"></script>
+<link rel="stylesheet" type="text/css" href="css/home_slider.css"/>
+<script type="text/javascript">
+    $(document).ready(function() {
+                            $("#slider").zAccordion({
+                                    tabWidth: "15%",
+                                    width: "100%",
+
+                                    height: "100%",
+                                    slideClass: 'slider',
+                                    animationStart: function () {
+                                        $('#slider').find('li.slider-open div').css('display', 'none');
+                                        $('#slider').find('li.slider-previous div').css('display', 'none');
+                                    },
+                                    animationComplete: function () {
+                                        $('#slider').find('li div').fadeIn(600);
+                                    }    
+                            });
+                            $(window).resize(function() {
+                                    $("#slider").height($(window).height());
+                                    $("#slider li").height($(window).height());
+                                    $("#slider img").height($(window).height());
+                            });
+
+
+
+                    });
+</script>
+
+<?php
+
+echo getFooter(); 
+
+?>
+
+		
--- restaurant_menukaart2.php
+++ restaurant_menukaart2.php
@@ -10,12 +10,7 @@
     <link href="css/prettyPhoto.css" rel="stylesheet"> 
     <link href="css/animate.min.css" rel="stylesheet"> 
 	<link href="css/main.css" rel="stylesheet">
-	<link href="css/responsive.css" rel="stylesheet">
-
-    <!--[if lt IE 9]>
-	    <script src="js/html5shiv.js"></script>
-	    <script src="js/respond.min.js"></script>
-    <![endif]-->       
+	<link href="css/responsive.css" rel="stylesheet">    
     <link rel="shortcut icon" href="images/ico/favicon.ico">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
