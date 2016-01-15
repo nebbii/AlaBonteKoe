fa334339d6f2663aa2259ae4dc7b7559e9def3df
nebbii <ben.wolthuis@gmail.com>
2016-01-15 10:21:55 +0100

    mv menukaart files and added from mnw_newcss

---
+++ restaurant_menukaart2.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet"> 
+    <link href="css/animate.min.css" rel="stylesheet"> 
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+	    <script src="js/html5shiv.js"></script>
+	    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+	<header id="header">      	
+        <div class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <a class="navbar-brand" href="index.html">
+                       <img src="images/logo3.png" class="img-responsive inline"  height="100" width="100" alt="">
+                    </a>
+                    
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="restaurant_home.php">Home</a></li>
+                        <li><a href="restaurant_menukaart.php">menukaart</a></li>                       
+                        <li><a href="restaurant_reserveren.php">reserveren</a></li>                       
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--/#header-->
+
+    <section class="container">        
+        <div class="price-table-2">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+					<img src="images/top1.png" class="img-responsive inline" alt="">
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-two">
+					<img src="images/top3.png" class="img-responsive inline" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-three">
+						<img src="images/top2.png" class="img-responsive inline" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  
+
+    <section class="container">        
+        <div class="price-table">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Voorgerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">Koude voorgerechten</span></p>
+                        </div>
+                        <ul>                            
+                            <li>Haringtartaar met tomaat en lente-ui<span>€ 3,50</span></li>
+                            <li>Stokbrood met kruidenboter	<span>€ 2,50</span></li>
+                            <li>Garnalen Cocktail	<span>€ 3,50</span></li>
+                            <li>Gemarineerde champignons met geitenkaas<span>€ 3,- </span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Voorgerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">Warme voorgerechten</span></p>
+                        </div>                        
+                        <ul>
+                            <li>Verse tomatensoep	 <span>€ 4,-</span></li>
+                            <li>Soep van de dag <span>€ 5,-</span></li>
+                            <li>Gefrituurde inktvis ringen<span>€ 6,50</span> </li>
+                            <li>Mini Hamburgers<span>€ 9,50</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow btn-hightlight"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Voorgerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">Salades</span></p>
+                        </div>
+                        <ul>
+                            <li>Groene Salade<span>	€ 4,-</span></li>
+                            <li>Salade met gerookte zalm <span>	€ 5,50</span></li>
+                            <li>Salade met gebakken scampi's <span>	€ 6,-</span> </li>
+                            <li>Salade met walnoten <span>	€ 5,-</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow"></a>
+                    </div>
+                </div>
+            </div>
+        </div><!--/#price-table-->
+		<h1>Alle vlees en vis hoofdgerechten worden geserveerd met patat en salade</h1>
+    </section>
+
+        <section class="container">        
+        <div class="price-table">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Hoofdgerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">Vlees</span></p>
+                        </div>
+                        <ul>                            
+                            <li>Ossenhaas met champignonensaus<span>	€ 11,-</span></li>
+                            <li>Wienerschnitzel	<span>€ 12,50</span></li>
+                            <li>Biefstuk<span>€ 12,-</span></li>
+                            <li>Spareribs (4 stuks)<span>	€ 13,-</span></li>
+                            <li>De bonte koe hamburger met patat<span>€ 12,50</span></li>
+                            <li>Kipfile<span>€ 9,50</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Hoofdgerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">Pizza's</span></p>
+                        </div>                        
+                        <ul>
+                            <li>Margarita<span>€8,-</span></li>
+                            <li>Peperoni <span>€9,50,-</span></li>
+                            <li>Tonijn <span>€10,-</span> </li>
+                            <li>Hawaii <span>€10,50</span></li>
+                            <li>Shoarma<span>€9,50</span></li>
+                            <li>De bonte koe pizza <span>€14</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow btn-hightlight"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Hoofdgerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">Vis</span></p>
+                        </div>
+                        <ul>
+                            <li>Groene Salade<span>	€ 4,-</span></li>
+                            <li>Salade met gerookte zalm <span>	€ 5,50</span></li>
+                            <li>Salade met gebakken scampi's <span>	€ 6,-</span> </li>
+                            <li>Salade met walnoten <span>	€ 5,-</span></li>
+                            <li>duidelijk ook een vis schotel <span>	€ 21,-</span></li>
+                            <li>haggis<span>€9,-</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow"></a>
+                    </div>
+                </div>
+            </div>
+        </div><!--/#price-table-->
+    </section>
+	
+	        <section class="container">        
+        <div class="price-table">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">nagerecht</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">ijs</span></p>
+                        </div>
+                        <ul>                            
+                            <li>Ijs ijs bbi<span>	€ 11,-</span></li>
+                            <li>Blue ijs	<span>€ 12,50</span></li>
+                            <li>white dragon<span>€ 12,-</span></li>
+                            <li>bacon<span>	€ 13,-</span></li>
+                            <li>Tears of thine enemies<span>€ 12,50</span></li>
+                            <li>weeaboo<span>€ 9,50</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Dranken</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">koude dranken</span></p>
+                        </div>                        
+                        <ul>
+                            <li>Drank A<span>€8,-</span></li>
+                            <li>Drank B <span>€9,50,-</span></li>
+                            <li>Drank C <span>€10,-</span> </li>
+                            <li>Drank D <span>€10,50</span></li>
+                            <li>Drank E<span>€9,50</span></li>
+                            <li>Drank Q <span>€14</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow btn-hightlight"></a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="single-price price-one">
+                        <div class="table-heading">
+                            <p class="plan-name">Dranken</p>
+                            <p class="plan-price"><span class="dollar-sign"></span><span class="price"></span><span class="month">warme dranken		</span></p>
+                        </div>
+                        <ul>
+                            <li>Drank A<span>€8,-</span></li>
+                            <li>Drank B <span>€9,50,-</span></li>
+                            <li>Drank C <span>€10,-</span> </li>
+                            <li>Drank D <span>€10,50</span></li>
+                            <li>Drank E<span>€9,50</span></li>
+                            <li>Drank Q <span>€14</span></li>
+                        </ul>
+                        <a href="#" class="btn btn-buynow"></a>
+                    </div>
+                </div>
+            </div>
+        </div><!--/#price-table-->
+    </section>
+	
+	    <section class="container">
+        
+        <div class="price-table-2">
+            <div class="row">
+                <div class="col-sm-6 col-md-3">
+                </div>
+                <div class="col-sm-6 col-md-6	">
+                    <div class="single-price price-two">
+					<img src="images/bot.png" class="img-responsive inline" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="single-price price-three">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center bottom-separator">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/#footer-->
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>   
+</body>
+</html>
