25debb60e092912d5b8ab2811bcfa12204ea3a54
nebbii <ben.wolthuis@gmail.com>
2016-01-19 12:53:35 +0100

    made new-style homepage, made secondary pages the primary ones

--- restaurant_home.php
+++ restaurant_home.php
@@ -1,18 +1,13 @@
 <?php 
 	include_once("classes/database.class.php");
 	include_once("include/config.php");
-	include_once "page_navigation.php";
-
-	echo getNav();
 
 	$conn = Database::getInstance();
 	$conn->connect(HOST,USER,PASS,DBNAME);
 
 	if((isset($_POST))&&$_POST!=null) 
 	{		
-		// build datetime
-		$datetime = explode("/",$_POST['date']);
-		$datetime = $datetime[2]."/".$datetime[1]."/".$datetime[0]." ".$_POST['tijd'];
+		$datetime = $_POST['date']." ".$_POST['tijd'];
 		
 		$sql = "INSERT INTO `reserveringen`(naam,aantalpers,date,opmerking) 
 			VALUES ('{$_POST['naam']}','{$_POST['aantalPers']}','{$datetime}','{$_POST['opmerking']}')";
@@ -20,187 +15,156 @@
 		$conn->doQuery($sql);
 		// prg
 		header("Location: " . $_SERVER['REQUEST_URI'] . "?q=submitform");
+
 		exit();
 	}
 ?>
-<body class="restaurant">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
 
-    <div class="banner_restaurant">
-        <div class="banner_div">
-             <a href="index.php"><img src="images/Logo/Logo3.png" class="banner_img" /></a>
-        </div>
-        <div class="banner_div">
-            <h1 class="banner_header">Restaurant Module</h1>
-        </div>
-        <div class="banner_buttons">
-            <a href="restaurant_home.php"><button>Home</button></a>
-            <a href="restaurant_menukaart.php"><button>Menukaart</button></a>
-            <a href="restaurant_reserveren.php"><button>Reserveren</button></a>
-        </div>
-    </div>
-    <div class="main_div" >
-		<?php 
-		if((isset($_GET['q']))&&$_GET['q']=='submitform') 
-		{
-		?>
-			<center><h2 class="main_header">Uw reservering is aangemaakt!</h2></center>
-			<br>
-			<center><p class="main_p">
-				U zal binnenkort de factuur en een confirmatie mailtje ontvangen waarin
-				de datum en prijs van de reservering staat.
-				</p>
-			</center>
-		<?php
-		}
-			else
-		{
-		?>
-        <center><h2 class="main_header">Welcome op de Restaurant Module</h2></center>
-        <br>
-        <center><p class="main_p">
-            Als u zoekt voor een restaurant om te eten, dan bent u op de juiste plek!!<br>
-            Want het restaurant van De Bonte Koe is echt de perfecte plek voor als je gaat uiteten, wij hebben alles 
-            van elke vleesproduct naar elke vis product, voor jong en oud.<br>
-            Natuurlijk mag uw huisdier ook mee want hebben ook een menukaart voor uw huisdier
-            want wij denken niet alleen aan onze klanten maar ook aan hun geliefde 
-            Wist u ook dat als u hier eet tegelijk ook korting krijgt bij ons in de bioscoop
-            Jazeker, bij elke bestelling zit een bepaalde kortings persentage vast
-            Dus hoe meer u besteld des te meer korting u krijgt
-            <br><br>
-            Dus waar wacht u nog op reserveer zosnel mogelijk bij De Bonte Koe 
+    <!--[if lt IE 9]>
+	    <script src="js/html5shiv.js"></script>
+	    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
 
-        </p>
-        </center>
-		<?php
-		}
-		?>
-        <br /><br /><br />
-        <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden; visibility: hidden;">
-            <!-- Loading Screen -->
-            <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-                <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-                <div style="position:absolute;display:block;background:url('images/restaurant_slider/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-            </div>
-            <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden;">
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/233C236F00000578-2837806-image-1_1416233939406.jpg" />
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">			
+			
+			<!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigatie">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+			</button>-->
+			<!--<div class="collapse navbar-collapse" id="navigatie">-->
+				<ul class="nav navbar-nav">
+					<li>
+					<a class="navbar-brand" href="index.php">
+						<img src="images/logo3.png" class="img-responsive inline"  height="100" width="100" alt="">
+					</a>
+					</li>
+					<li><a href="restaurant_home.php">Home</a></li>
+					<li><a href="restaurant_menukaart.php">Menukaart</a></li>                       
+					<li><a href="restaurant_reserveren.php">Reserveren</a></li>                       
+				</ul>
+			<!--</div>-->
+		</div>
+	</nav><br />
+    <!--/#header-->
+    <section class="container">        
+        <div class="price-table-2">
+            <div class="row">
+                <div class="col-sm-4">
+					<img src="images/top1.png" class="img-responsive inline" alt="">
                 </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-Sticky-Toffee-Pudding.jpg" />
+                <div class="col-sm-4">
+                    <div class="single-price price-two">
+					<img src="images/tophome.png" class="img-responsive inline" alt="Homepagina (image moet gemaakt worden!)">
+                    </div>
                 </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-cheese-burger.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-interior.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-la-restaurant.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-moo-bar (1).jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-restaurant.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-sausage-burrata-pizza.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_fat-cow-squash-blossom-tempura.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_gordon-ramsay-the-fat-cow-restaurant.jpg" />
-                </div>
-                <div data-p="112.50" style="display: none;">
-                    <img data-u="image" src="images/restaurant_slider/images_blog_images_2_b2ap3_thumbnail_the-fat-cow-la-corina_20121210-234559_1.jpg" />
+                <div class="col-sm-4">
+                    <div class="single-price price-three">
+						<img src="images/top2.png" class="img-responsive inline" alt="">
+                    </div>
                 </div>
             </div>
-        
-            <div data-u="navigator" class="jssorb05" style="bottom:16px;right:6px;" data-autocenter="1">
-                <!-- bullet navigator item prototype -->
-                <div data-u="prototype" style="width:16px;height:16px;"></div>
-            </div>
-            <!-- Arrow Navigator -->
-            <span data-u="arrowleft" class="jssora12l" style="top:123px;left:0px;width:30px;height:46px;" data-autocenter="2"></span>
-            <span data-u="arrowright" class="jssora12r" style="top:123px;right:0px;width:30px;height:46px;" data-autocenter="2"></span>
         </div>
-        <br /><br />
-    </div>
+    </section>
 
-        <!-- Side Menu -->    
-    <div class="side_div">
-        <br />
-        <p class="side_p"><b>
-            <br />
-            Dagelijks open van 16:00 t/m 23:00
-            </b>
-            <br /><br /><br /><br />
-            Uitgaanscentrum De Bonte Koe<br />
-            Dorpsstraat 14b<br />
-            2481BA Woubrugge<br />
-            0172-518274
-            <br /><br /><br /><br />
-            <b>Website</b><br />
-            <small class="link"><a href="">http://uitgaanscentrumdebontekoe.nl/restaurant</a></small><br />
-            <br />
-            <b>E-mail</b><br />
-            <small class="link"><a href="">restaurant@uitgaancentrumdebontekoe.nl</a></small>      
-        </p> 
-        <br />
-    </div>
+
+    <section id="blog-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="row">
+                         <div class="col-md-8 col-sm-12">
+                            <div class="single-blog blog-details two-column">
+                                <div class="post-thumb">
+                                    
+                                </div>
+								<div class="main_div" >
+									<?php 
+									if((isset($_GET['q']))&&$_GET['q']=='submitform') 
+									{
+									?>
+										<h1 class="main_header">Uw reservering is aangemaakt!</h1>
+										<br>
+										<p class="main_p">
+											U zal binnenkort de factuur en een confirmatie mailtje ontvangen waarin
+											de datum en prijs van de reservering staat.
+											</p>
+										
+									<?php
+									}
+										else
+									{
+									?>
+									<h1 class="main_header">Welcome op de Restaurant Module.</h1>
+									<br>
+									<p class="main_p">
+										Als u zoekt voor een restaurant om te eten, dan bent u op de juiste plek!!<br>
+										Want het restaurant van De Bonte Koe is echt de perfecte plek voor als je gaat uiteten, wij hebben alles 
+										van elke vleesproduct naar elke vis product, voor jong en oud.<br>
+										Natuurlijk mag uw huisdier ook mee want hebben ook een menukaart voor uw huisdier
+										want wij denken niet alleen aan onze klanten maar ook aan hun geliefde 
+										Wist u ook dat als u hier eet tegelijk ook korting krijgt bij ons in de bioscoop
+										Jazeker, bij elke bestelling zit een bepaalde kortings persentage vast
+										Dus hoe meer u besteld des te meer korting u krijgt
+										<br><br>
+										Dus waar wacht u nog op reserveer zosnel mogelijk bij De Bonte Koe 
+
+									</p>
+									
+									<?php
+									}
+									?>
+								</div>
+							</div>
+                        </div>
+                    </div>
+                </div></br></br>	
+                <div class="col-md-3 col-sm-4">
+                    <div class="sidebar blog-sidebar">
+                        <div class="sidebar-item  recent"> 
+							<br>
+							<h3>Dagelijks open van 16:00 t/m 23:00</h3>
+							</b>
+							<h3> Uitgaanscentrum De Bonte Koe<br>
+							Dorpsstraat 14b<br>
+							2481BA Woubrugge<br>
+							0172-518274</h3>
+							<br><br><br><br>
+							<b>Website</b><br>
+							   <h3><small class="link"><a href="">http://uitgaanscentrumdebontekoe.nl/restaurant</a></small><br></h3>
+							<br>
+							<b>E-mail</b><br>
+							   <h3><small class="link"><a href="">restaurant@uitgaancentrumdebontekoe.nl</a></small>    </h3>     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
-    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-    <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
-    <script>
-        jQuery(document).ready(function ($) {
-            
-            var jssor_1_SlideshowTransitions = [
-              { $Duration:1200,$Opacity:2 }
-            ];
-            
-            var jssor_1_options = {
-              $AutoPlay: true,
-              $SlideshowOptions: {
-                $Class: $JssorSlideshowRunner$,
-                $Transitions: jssor_1_SlideshowTransitions,
-                $TransitionsOrder: 1
-              },
-              $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$
-              },
-              $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$
-              }
-            };
-            
-            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-            
-            //responsive code begin
-            //you can remove responsive code if you don't want the slider scales while window resizes
-            function ScaleSlider() {
-                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-                if (refSize) {
-                    refSize = Math.min(refSize, 600);
-                    jssor_1_slider.$ScaleWidth(refSize);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
-            ScaleSlider();
-            $(window).bind("load", ScaleSlider);
-            $(window).bind("resize", ScaleSlider);
-            $(window).bind("orientationchange", ScaleSlider);
-            //responsive code end
-        });
-    </script>
-    
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lightbox.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>   
 </body>
 </html>
-
-<?php
-
-echo getFooter(); 
-
-?>
--- restaurant_reserveren.php
+++ restaurant_reserveren.php
@@ -1,131 +1,145 @@
-<?php 
-include_once('include/config.php');
-include_once('classes/database.class.php');
-include_once('classes/table.class.php');
-include_once('classes/reservering.class.php');
-include_once "page_navigation.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
 
-if (!empty($_POST))
-{
-    $dbo = Database::getInstance();
-    $dbo->connect(HOST, GEBRUIKER, WACHTWOORD, DATABASE);
+    <!--[if lt IE 9]>
+	    <script src="js/html5shiv.js"></script>
+	    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
 
-    $reservering = new Reservering();
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">			
+			
+			<!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigatie">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+			</button>-->
+			<!--<div class="collapse navbar-collapse" id="navigatie">-->
+				<ul class="nav navbar-nav">
+					<li>
+					<a class="navbar-brand" href="index.php">
+						<img src="images/logo3.png" class="img-responsive inline"  height="100" width="100" alt="">
+					</a>
+					</li>
+					<li><a href="restaurant_home.php">Home</a></li>
+					<li><a href="restaurant_menukaart.php">Menukaart</a></li>                       
+					<li><a href="restaurant_reserveren.php">Reserveren</a></li>                       
+				</ul>
+			<!--</div>-->
+		</div>
+	</nav><br />
+    <!--/#header-->
+    <section class="container">        
+        <div class="price-table-2">
+            <div class="row">
+                <div class="col-sm-4">
+					<img src="images/top1.png" class="img-responsive inline" alt="">
+                </div>
+                <div class="col-sm-4">
+                    <div class="single-price price-two">
+					<img src="images/top4.png" class="img-responsive inline" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="single-price price-three">
+						<img src="images/top2.png" class="img-responsive inline" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    $data = $_POST;
-    $reservering->bind($data);
-    $reservering->store();
 
-    header('Location:restaurant_reserveren.php');
+    <section id="blog-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="row">
+                         <div class="col-md-12 col-sm-8">
+                            <div class="single-blog blog-details two-column">
+                                <div class="post-thumb">
+                                    
+                                </div>
+								<form name="reserveren" action="restaurant_home2.php" method="post">
+									<table class="reserveer_table">
+										<tr>
+											<th>Naam:</th>
+											<td><input type="text" name="naam" class="reserveer_input"  required></td>
+										</tr>
+										<tr>
+											<th>E-mailadres:</th>
+											<td><input type="email" name="email" class="reserveer_input" required></td>
+										</tr>
+										<tr>
+											<th>Datum:</th>
+											<td><input  type="text" name="date" class="reserveer_input"  id="datepicker" required></td>
+										</tr>
+										<tr>
+											<th>Tijd:</th>
+											<td><input type="time" name="tijd" class="reserveer_input" required></td>
+										</tr>
+										<tr>
+											<th>Aantal Personen:</th>
+											<td><input type="number" name="aantalPers" class="reserveer_input" required></td>
+										</tr>
 
-}
-else {
+										<tr>
+											<th>Opmerking:</th>
+											<td><textarea rows="4" name="opmerking" class="reserveer_textarea"></textarea></td>
+										</tr>
+										<tr>
+											<td colspan="2">
+											<input type="submit" name="submit" value="Reserveer"></td>
+										</tr>
+									</table>
+								</form>
+                            </div>
+                        </div>
+                    </div>
+                </div></br></br>	
+                <div class="col-md-3 col-sm-12">
+                    <div class="sidebar blog-sidebar">
+                        <div class="sidebar-item  recent"> 
+							<br>
+							<h3>Dagelijks open van 16:00 t/m 23:00</h3>
+							</b>
+							<h3> Uitgaanscentrum De Bonte Koe<br>
+							Dorpsstraat 14b<br>
+							2481BA Woubrugge<br>
+							0172-518274</h3>
+							<br><br><br><br>
+							<b>Website</b><br>
+							   <h3><small class="link"><a href="">http://uitgaanscentrumdebontekoe.nl/restaurant</a></small><br></h3>
+							<br>
+							<b>E-mail</b><br>
+							   <h3><small class="link"><a href="">restaurant@uitgaancentrumdebontekoe.nl</a></small>    </h3>     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
-    echo getNav();
-?>
 
-<body class="restaurant">
-	<div class="banner_restaurant">
-		<div class="banner_div">
-			<a href="index.php"><img src="images/Logo/Logo3.png" class="banner_img" /></a>
-		</div>
-		<div class="banner_div">
-			<h1 class="banner_header">Restaurant Module</h1>
-		</div>
-		<div class="banner_buttons">
-			<a href="restaurant_home.php"><button>Home</button></a>
-			<a href="restaurant_menukaart.php"><button>Menukaart</button></a>
-			<a href="#"><button>Reserveren</button></a>
-		</div>
-	</div>
-	<div class="main_div" >
-		<center><h2 class="main_header">Reserveren</h2></center>
-		<br>
-		<p class="main_p">
-			<form name="reserveren" action="restaurant_home.php" method="post">
-				<table class="reserveer_table">
-					<tr>
-						<td>Naam:<span style="color:red">*</span></td>
-						<td><input type="text" name="naam" class="reserveer_input"  required></td>
-					</tr>
-					<tr>
-						<td>E-mailadres:<span style="color:red">*</span></td>
-						<td><input type="email" name="email" class="reserveer_input" required></td>
-					</tr>
-					<tr>
-						<td>Datum:<span style="color:red">*</span></td>
-						<td><input  type="text" name="date" class="reserveer_input"  id="datepicker" required></td>
-					</tr>
-					<tr>
-						<td>Tijd:<span style="color:red">*</span></td>
-						<td><input type="time" name="tijd" class="reserveer_input" required></td>
-					</tr>
-					<tr>
-						<td>Aantal Personen:<span style="color:red">*</span></td>
-						<td><input type="number" name="aantalPers" class="reserveer_input" required></td>
-					</tr>
-
-					<tr>
-						<td>Opmerking:</td>
-						<td><textarea rows="4" name="opmerking" class="reserveer_textarea"></textarea></td>
-					</tr>
-					<tr>
-						<td colspan="2">
-						<input type="submit" name="submit" value="Reserveer"></td>
-					</tr>
-				</table>
-			</form>
-			
-		</p>
-	   
-		<br /><br /><br />
-
-			
-			
-		<br /><br />
-	</div>
-	<?php
-
-		}
-
-	?>
-		
-		<!-- Side Menu -->
-	<div class="side_div">
-		<br />
-		<p class="side_p"><b>
-			<br />
-			Dagelijks open van 16:00 t/m 23:00
-			</b>
-			<br /><br /><br /><br />
-			Uitgaanscentrum De Bonte Koe<br />
-			Dorpsstraat 14b<br />
-			2481BA Woubrugge<br />
-			0172-518274
-			<br /><br /><br /><br />
-			<b>Website</b><br />
-			<small class="link"><a href="">http://uitgaanscentrumdebontekoe.nl/restaurant</a></small><br />
-			<br />
-			<b>E-mail</b><br />
-			<small class="link"><a href="">restaurant@uitgaancentrumdebontekoe.nl</a></small>      
-		</p> 
-		<br />
-	</div>
-		
-		
-	<link rel="stylesheet" href="css/datepicker.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<script src="js/jquery-1.9.1.min.js"></script>
-			<script src="js/bootstrap-datepicker.js"></script>
-			<script type="text/javascript">
-				// When the document is ready
-				$(document).ready(function () {
-					$('#datepicker').datepicker({
-						format: "dd/mm/yyyy",
-					}).datepicker("setDate", new Date());
-				});
-			</script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lightbox.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>   
 </body>
 </html>
-    
-
