d304233b20e1b707812f592b818e888fcbdc5b05
AZNLiLPiNAY <cyndi_pinay@hotmail.com>
2016-02-26 10:53:42 +0100

    Contact Pagina is verbeterd (nog geen tekst voor Bioscoop)

--- contact.php
+++ contact.php
@@ -12,6 +12,68 @@ echo getNav();
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#flip").click(function(){
+                $("#panel").slideToggle("slow");
+            });
+        });
+
+        $(document).ready(function(){
+            $("#flip2").click(function(){
+                $("#panel2").slideToggle("slow");
+            });
+        });
+
+        $(document).ready(function(){
+            $("#flip3").click(function(){
+                $("#panel3").slideToggle("slow");
+            });
+        });
+    </script>
+
+    <style>
+        #panel, #flip {
+            padding: 5px;
+            text-align: center;
+            background-color: #D8267D;
+            font-family: "Shadows Into Light Two";
+        }
+
+        #panel {
+            padding: 30px;
+            display: none;
+            font-size: 21px;
+        }
+
+        #panel2, #flip2 {
+            padding: 5px;
+            text-align: center;
+            background-color: #41BCEB;
+            font-family: "Shadows Into Light Two";
+        }
+
+        #panel2 {
+            padding: 30px;
+            display: none;
+            font-size: 21px;
+        }
+
+        #panel3, #flip3 {
+            padding: 5px;
+            text-align: center;
+            background-color: #FF6A00;
+            font-family: "Shadows Into Light Two";
+        }
+
+        #panel3 {
+            padding: 30px;
+            display: none;
+            font-size: 21px;
+        }
+    </style>
 </head>
 
 <body class="contact">
@@ -21,13 +83,19 @@ echo getNav();
     </div>
     <div class="banner_div">
         <h1 class="banner_header"><strong>Contact Pagina</strong></h1>
+        <div class="banner_header"><small>Klik op de contact titels</small></div>
     </div>
-
 </div>
+
 <div class="contact_div">
-    <div class="contact_restaurant"><center><p class="contact_p">Contact gegevens Restaurant</p></center></div>
-    <div class="contact_uitgaan"><center><p class="contact_p">Contact gegevens Uitgaan</p></center></div>
-    <div class="contact_bioscoop"><center><p class="contact_p">Contact gegevens Bioscoop</p></center></div>
+    <div class="contact_restaurant"><center><p class="contact_p"><div id="flip"><h1>Contact gegevens Restaurant</h1></div></p></center>
+        <div id="panel">Dagelijks open van &nbsp; 16:00uur t/m 23:00uur <hr> Uitgaanscentrum &nbsp;&nbsp;&nbsp;&nbsp;De Bonte Koe &nbsp; Dorpsstraat 14b &nbsp; 2481BA Woubrugge &nbsp;0172-518274</div></div>
+
+    <div class="contact_uitgaan"><center><p class="contact_p"><div id="flip2"><h1>Contact gegevens Uitgaan</h1></div></p></center>
+        <div id="panel2">Openingstijden: &nbsp;&nbsp; Vrijdag & Zaterdag &nbsp; 16:00uur t/m 02:00uur <hr>&nbsp; Toegangsprijs &nbsp; &euro;7,50 euro (inc. btw) &nbsp; 3 zalen: 80's en 90's, Urban & Schuurfeest!</div></div>
+
+    <div class="contact_bioscoop"><center><p class="contact_p"><div id="flip3"><h1>Contact gegevens Bioscoop</h1></div></p></center>
+        <div id="panel3">Elke snorhaar is welkom</div></div>
 </div>
 
 <script>
